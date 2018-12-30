4f143c5cfb31dff67f3109e5ea36f824a7958879
LaughingLeader <laughingleader@gmail.com>
2018-12-30 08:59:07 -0600

    Use the project name + UUID instead of the FolderName, as the folder name may be different (imported classic projects)

--- SCG.Modules/DOS2DE/Core/DOS2DEProjectController.cs
+++ SCG.Modules/DOS2DE/Core/DOS2DEProjectController.cs
@@ -692,7 +692,7 @@ namespace SCG.Core
 			}
 
 			//string inputDirectory = Path.Combine(Path.GetFullPath(Data.Settings.GitRootDirectory), modProject.ProjectName);
-			string outputPackage = Path.ChangeExtension(Path.Combine(outputDirectory, modProject.FolderName), "pak");
+			string outputPackage = Path.ChangeExtension(Path.Combine(outputDirectory, modProject.ProjectName + "_" + modProject.ModuleInfo.UUID), "pak");
 			try
 			{
 				var sourceFolders = new List<string>();
