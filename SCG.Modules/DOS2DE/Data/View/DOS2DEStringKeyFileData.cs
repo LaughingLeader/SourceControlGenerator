ecb39007cc89e3e29729a488618f3e50261d8e51
LaughingLeader <laughingleader@gmail.com>
2019-05-24 15:47:12 -0500

    Added some initial LSB parsing for localization editing

---
+++ SCG.Modules/DOS2DE/Data/View/DOS2DEStringKeyFileData.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SCG.Data;
+using System.Collections.ObjectModel;
+
+namespace SCG.Modules.DOS2DE.Data.View
+{
+	public class DOS2DELocalizationViewData:PropertyChangedBase
+	{
+		public ObservableCollection<DOS2DEStringKeyFileData> ModsLocalization { get; set; }
+
+		public ObservableCollection<DOS2DEStringKeyFileData> PublicLocalization { get; set; }
+	}
+
+	public class DOS2DEStringKeyFileData
+	{
+		public LSLib.LS.Resource Source { get; private set; }
+
+		public List<DOS2DEKeyEntry> Entries { get; set; }
+
+		public DOS2DEStringKeyFileData(LSLib.LS.Resource res)
+		{
+			Source = res;
+
+			Entries = new List<DOS2DEKeyEntry>();
+
+			try
+			{
+				var rootNode = res.Regions.First().Value;
+				foreach(var entry in rootNode.Children)
+				{
+					foreach(var node in entry.Value)
+					{
+						DOS2DEKeyEntry localeEntry = new DOS2DEKeyEntry(node);
+						Entries.Add(localeEntry);
+					}
+					
+				}
+			}
+			finally { }
+		}
+
+		private void TraceNodes(KeyValuePair<string, List<LSLib.LS.Node>> keyValuePair, int indent = 0)
+		{
+			Log.Here().Activity($"{String.Concat(Enumerable.Repeat("\t", indent))}Key[{keyValuePair.Key}]|Value[{keyValuePair.Value}]");
+			foreach (var next in keyValuePair.Value)
+			{
+				TraceNode(next, indent + 1);
+			}
+		}
+
+		private void TraceNode(LSLib.LS.Node v, int indent = 0)
+		{
+			Log.Here().Activity($"{String.Concat(Enumerable.Repeat("\t", indent))}Name[{v.Name}]|{v.GetType()}|Parent: [{v.Parent.Name}]");
+			foreach (var att in v.Attributes)
+			{
+				TraceAtt(att, indent + 1);
+			}
+			foreach (var child in v.Children)
+			{
+				TraceNodes(child, indent + 1);
+			}
+		}
+
+		private void TraceAtt(KeyValuePair<string, LSLib.LS.NodeAttribute> attdict, int indent = 0)
+		{
+			Log.Here().Activity($"{String.Concat(Enumerable.Repeat("\t", indent))}Attribute: {attdict.Key} | {attdict.Value.Type} = {attdict.Value.Value}");
+		}
+	}
+
+	public class DOS2DEKeyEntry : SCG.Data.PropertyChangedBase
+	{
+		private LSLib.LS.Node node;
+
+		private LSLib.LS.NodeAttribute uuidNode;
+		private LSLib.LS.NodeAttribute contentNode;
+
+		public string Key
+		{
+			get { return uuidNode != null ? uuidNode.Value.ToString() : ""; }
+			set
+			{
+				node.Attributes["UUID"].Value = value;
+				RaisePropertyChanged("Key");
+			}
+		}
+
+		public string Content
+		{
+			get { return contentNode != null ? contentNode.Value.ToString() : ""; }
+			set
+			{
+				node.Attributes["Content"].Value = value;
+				RaisePropertyChanged("Content");
+			}
+		}
+
+		public DOS2DEKeyEntry(LSLib.LS.Node resNode)
+		{
+			node = resNode;
+
+			uuidNode = node.Attributes["UUID"];
+			contentNode = node.Attributes["Content"];
+		}
+	}
+}
---
+++ SCG.Modules/DOS2DE/Utilities/DOS2DELocalizationEditor.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Alphaleonis.Win32;
+using Alphaleonis.Win32.Filesystem;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using LSLib.LS;
+using LSLib.LS.Enums;
+using SCG.Core;
+using SCG.Data.App;
+using SCG.Modules.DOS2DE.Data.View;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Collections.ObjectModel;
+
+namespace SCG.Modules.DOS2DE.Utilities
+{
+	public class DOS2DELocalizationEditor
+	{
+		public static async Task<DOS2DELocalizationViewData> LoadStringKeyData(string dataRootPath, ModProjectData modProjectData, CancellationToken? token = null)
+		{
+			DOS2DELocalizationViewData localizationData = new DOS2DELocalizationViewData();
+			try
+			{
+				if (token == null) token = CancellationToken.None;
+
+				if (token.Value.IsCancellationRequested)
+				{
+					Log.Here().Warning($"Localization loading cancellation requested.");
+					return localizationData;
+				}
+
+				if (!dataRootPath.EndsWith(Path.DirectorySeparatorChar.ToString()))
+				{
+					dataRootPath += Path.DirectorySeparatorChar;
+				}
+
+				string modsLocalePath = Path.Combine(Path.GetFullPath(Path.Combine(dataRootPath, "Mods")), modProjectData.FolderName, "Localization");
+				string publicLocalePath = Path.Combine(Path.GetFullPath(Path.Combine(dataRootPath, "Public")), modProjectData.FolderName, "Localization");
+
+				bool modsExists = Directory.Exists(modsLocalePath);
+				bool publicExists = Directory.Exists(publicLocalePath);
+
+				if (!modsExists && !publicExists)
+				{
+					Log.Here().Warning($"Failed to find localization folders for mod {modProjectData.DisplayName} at path '{modsLocalePath}' and '{publicLocalePath}'.");
+					return localizationData;
+				}
+
+				if(modsExists)
+				{
+					Log.Here().Activity($"Loading localization data from '{modsLocalePath}'.");
+					var modsLocaleData = await LoadLSBFiles(modsLocalePath, token);
+					localizationData.ModsLocalization = new ObservableCollection<DOS2DEStringKeyFileData>(modsLocaleData);
+				}
+
+				if (publicExists)
+				{
+					Log.Here().Activity($"Loading localization data from '{publicLocalePath}'.");
+					var publicLocaleData = await LoadLSBFiles(publicLocalePath, token);
+					localizationData.PublicLocalization = new ObservableCollection<DOS2DEStringKeyFileData>(publicLocaleData);
+				}
+
+				return localizationData;
+			}
+			catch (Exception ex)
+			{
+				if (!token.Value.IsCancellationRequested)
+				{
+					Log.Here().Error($"Error creating package: {ex.ToString()}");
+				}
+				else
+				{
+					Log.Here().Important($"Cancelled creating package: {ex.ToString()}");
+				}
+				return localizationData;
+			}
+		}
+
+		private static async Task<List<DOS2DEStringKeyFileData>> LoadLSBFiles(string directoryPath, CancellationToken? token = null)
+		{
+			List<DOS2DEStringKeyFileData> stringKeyData = new List<DOS2DEStringKeyFileData>();
+
+			var filters = new DirectoryEnumerationFilters()
+			{
+				InclusionFilter = f =>
+				{
+					return Path.GetExtension(f.FileName).Equals("lsb", StringComparison.OrdinalIgnoreCase);
+				},
+				ErrorFilter = delegate (int errorCode, string errorMessage, string pathProcessed)
+				{
+					var gotException = errorCode == 5;
+
+					if (gotException)
+					{
+						Log.Here().Error($"Error reading file at '{pathProcessed}': [{errorCode}]({errorMessage})");
+					}
+
+					return gotException;
+				},
+				RecursionFilter = f =>
+				{
+					return true;
+				}
+			};
+			if (token != null) filters.CancellationToken = token.Value;
+			var lsbFiles = Directory.EnumerateFiles(directoryPath, Alphaleonis.Win32.Filesystem.DirectoryEnumerationOptions.Recursive, filters);
+			var targetFiles = new ConcurrentBag<string>(lsbFiles);
+			foreach (var filePath in targetFiles)
+			{
+				var data = await LoadLSB(filePath);
+				stringKeyData.Add(data);
+			}
+			return stringKeyData;
+		}
+
+		public static async Task<DOS2DEStringKeyFileData> LoadLSB(string path, CancellationToken? token = null)
+		{
+			return await Task.Run(() =>
+			{
+				var resource = LSLib.LS.ResourceUtils.LoadResource(path, ResourceFormat.LSB);
+
+				var data = new DOS2DEStringKeyFileData(resource);
+
+				foreach (var entry in data.Entries)
+				{
+					Log.Here().Activity($"Entry: Key[{entry.Key}] = '{entry.Content}'");
+				}
+
+				return data;
+			});
+		}
+	}
+}
