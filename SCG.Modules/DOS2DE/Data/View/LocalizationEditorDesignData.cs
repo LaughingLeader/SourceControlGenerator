db8ff8766c433b34e23822f93a500a7bf0ad98ac
LaughingLeader <laughingleader@gmail.com>
2019-05-30 19:42:09 -0500

    Swapped design data to be a child of the original data, for easier binding testing,

--- SCG.Modules/DOS2DE/Data/View/LocalizationEditorDesignData.cs
+++ SCG.Modules/DOS2DE/Data/View/LocalizationEditorDesignData.cs
@@ -12,52 +12,37 @@ using SCG.Modules.DOS2DE.Utilities;
 
 namespace SCG.Modules.DOS2DE.Data.View
 {
-	public class LocalizationEditorDesignData
+	public class LocalizationEditorDesignData : DOS2DELocalizationViewData
 	{
-		public DOS2DELocalizationViewData Data { get; set; }
 
-		public string Name { get; set; } = "Test";
-
-		public ObservableCollection<DOS2DELocalizationGroup> Groups { get; set; }
-
-		public LocaleMenuData MenuData { get; set; }
-
-		public LocalizationEditorDesignData()
+		public LocalizationEditorDesignData() : base()
 		{
-			Data = new DOS2DELocalizationViewData();
-
-			var metaFile = new FileInfo(@"G:\Divinity Original Sin 2\DefEd\Data\Mods\Nemesis_627c8d3a-7e6b-4fd2-8ce5-610d553fdbe9\meta.lsx");
-			ModProjectData testData = new ModProjectData(metaFile, @"G:\Divinity Original Sin 2\DefEd\Data\Projects");
+			//var metaFile = new FileInfo(@"G:\Divinity Original Sin 2\DefEd\Data\Mods\Nemesis_627c8d3a-7e6b-4fd2-8ce5-610d553fdbe9\meta.lsx");
+			//ModProjectData testData = new ModProjectData(metaFile, @"G:\Divinity Original Sin 2\DefEd\Data\Projects");
 
 			//var result = LoadStringKeyData(@"G:\Divinity Original Sin 2\DefEd\Data\", testData);
 			//Data = result.Data;
 			//Name = result.Error;
 
-			Name = "Test";
-			Data.ModsGroup.DataFiles = new ObservableRangeCollection<IKeyFileData>();
-			Data.ModsGroup.DataFiles.Add(new DOS2DEStringKeyFileDataBase("Skills"));
-			Data.ModsGroup.DataFiles.Add(new DOS2DEStringKeyFileDataBase("Statuses"));
-			Data.ModsGroup.DataFiles.Add(new DOS2DEStringKeyFileDataBase("Potions"));
+			ModsGroup.DataFiles.Add(new DOS2DEStringKeyFileDataBase("Skills"));
+			ModsGroup.DataFiles.Add(new DOS2DEStringKeyFileDataBase("Statuses"));
+			ModsGroup.DataFiles.Add(new DOS2DEStringKeyFileDataBase("Potions"));
 
-			Data.PublicGroup.DataFiles = new ObservableRangeCollection<IKeyFileData>();
-			Data.PublicGroup.DataFiles.Add(new DOS2DEStringKeyFileDataBase("Skills"));
-			Data.PublicGroup.DataFiles.Add(new DOS2DEStringKeyFileDataBase("Statuses"));
-			Data.PublicGroup.DataFiles.Add(new DOS2DEStringKeyFileDataBase("Potions"));
+			PublicGroup.DataFiles.Add(new DOS2DEStringKeyFileDataBase("Skills"));
+			PublicGroup.DataFiles.Add(new DOS2DEStringKeyFileDataBase("Statuses"));
+			PublicGroup.DataFiles.Add(new DOS2DEStringKeyFileDataBase("Potions"));
 
-			foreach(var d in Data.ModsGroup.DataFiles)
+			foreach(var d in ModsGroup.DataFiles)
 			{
 				DOS2DELocalizationEditor.Debug_CreateEntries(d.Entries);
 			}
 
-			foreach (var d in Data.PublicGroup.DataFiles)
+			foreach (var d in PublicGroup.DataFiles)
 			{
 				DOS2DELocalizationEditor.Debug_CreateEntries(d.Entries);
 			}
 
-			Data.UpdateCombinedGroup(true);
-
-			Groups = Data.Groups;
-			MenuData = Data.MenuData;
+			UpdateCombinedGroup(true);
 		}
 	}
 }
