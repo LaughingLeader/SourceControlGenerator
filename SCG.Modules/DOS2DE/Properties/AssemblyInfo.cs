71d680a6c203ca2f4b8aec0461c2d9e01a82218b
LaughingLeader <laughingleader@gmail.com>
2019-07-05 17:08:48 -0500

    Disabled the xceed localization dlls (anti-clutter)

--- SCG.Modules/DOS2DE/Properties/AssemblyInfo.cs
+++ SCG.Modules/DOS2DE/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Windows;
 [assembly: AssemblyCopyright("Copyright © LaughingLeader 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
+[assembly: System.Resources.NeutralResourcesLanguageAttribute("en")]
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
