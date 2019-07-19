476ac8812536813cafa78b3f88f18472a11f1998
LaughingLeader <laughingleader@gmail.com>
2019-07-19 15:38:47 -0500

    Added some color extensions for getting specific strings out of System.Windows.Media.Color

---
+++ SourceControlGenerator/Extensions/ColorExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Windows.Media
+{
+	public static class ColorExtensions
+	{
+		public static string ToHexString(this System.Windows.Media.Color c)
+		{
+			return "#" + c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2");
+		}
+
+		public static string ToRGBString(this System.Windows.Media.Color c)
+		{
+			return "RGB(" + c.R.ToString() + "," + c.G.ToString() + "," + c.B.ToString() + ")";
+		}
+	}
+}
