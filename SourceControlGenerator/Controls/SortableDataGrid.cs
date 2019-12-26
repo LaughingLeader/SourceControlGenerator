4d6b0e3fa44368eab6a4c401c5d9f7a40d0d02da
LaughingLeader <laughingleader@gmail.com>
2019-12-26 15:41:23 -0600

    Crash fix when changing group tabs

--- SourceControlGenerator/Controls/SortableDataGrid.cs
+++ SourceControlGenerator/Controls/SortableDataGrid.cs
@@ -28,22 +28,26 @@ namespace SCG.Controls
             base.OnItemsSourceChanged(oldValue, newValue);
 
             ICollectionView view = CollectionViewSource.GetDefaultView(newValue);
-            view.SortDescriptions.Clear();
-
-            // reset SortDescriptions for new ItemSource
-            if (m_SortDescriptions.ContainsKey(newValue))
+            if(view != null)
             {
-                foreach (SortDescription sortDescription in m_SortDescriptions[newValue])
-                {
-                    view.SortDescriptions.Add(sortDescription);
+                view.SortDescriptions.Clear();
 
-                    // I need to tell the column its SortDirection,
-                    // otherwise it doesn't draw the triangle adornment
-                    DataGridColumn column = Columns.FirstOrDefault(c => c.SortMemberPath == sortDescription.PropertyName);
-                    if (column != null)
-                        column.SortDirection = sortDescription.Direction;
+                // reset SortDescriptions for new ItemSource
+                if (m_SortDescriptions.ContainsKey(newValue))
+                {
+                    foreach (SortDescription sortDescription in m_SortDescriptions[newValue])
+                    {
+                        view.SortDescriptions.Add(sortDescription);
+
+                        // I need to tell the column its SortDirection,
+                        // otherwise it doesn't draw the triangle adornment
+                        DataGridColumn column = Columns.FirstOrDefault(c => c.SortMemberPath == sortDescription.PropertyName);
+                        if (column != null)
+                            column.SortDirection = sortDescription.Direction;
+                    }
                 }
             }
+            
             UpdateIndices();
         }
 
