d09f46fb6748203071968969735fdb78cb0b57d8
LaughingLeader <laughingleader@gmail.com>
2020-02-17 16:41:55 -0600

    Upped version

--- SourceControlGenerator/Properties/AssemblyInfo.cs
+++ SourceControlGenerator/Properties/AssemblyInfo.cs
@@ -20,5 +20,5 @@ using System.Windows;
     ResourceDictionaryLocation.None,
     ResourceDictionaryLocation.SourceAssembly
 )]
-[assembly: AssemblyVersion("1.6.2.0")]
-[assembly: AssemblyFileVersion("1.6.2.0")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
