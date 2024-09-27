79d268b9bbd11821d8bbf35a434c8aa590c7b05f
LaughingLeader <laughingleader@gmail.com>
2024-09-27 17:36:26 -0500

    Manually change to SDK-style csproj

---
+++ SourceControlGenerator/Core/DefaultResources.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCG;
+public static class DefaultResources
+{
+	public static readonly string License_MIT = @"";
+}
