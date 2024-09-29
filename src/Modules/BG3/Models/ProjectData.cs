0cbd03aab013afe2eea8bb2bf0d9182a92379b9f
LaughingLeader <laughingleader@gmail.com>
2024-09-29 16:24:06 -0500

    Add modified/created dates

--- src/Modules/BG3/Models/ProjectData.cs
+++ src/Modules/BG3/Models/ProjectData.cs
@@ -1,5 +1,6 @@
 ﻿using LSLib.LS;
 
+using System.Globalization;
 using System.Windows;
 using System.Windows.Media.Imaging;
 
@@ -16,10 +17,16 @@ public class ProjectData : ReactiveObject
 	[Reactive] public string? ModMetaFilePath { get; set; }
 	[Reactive] public ModuleData? Mod { get; set; }
 	[Reactive] public BitmapImage? Thumbnail { get; private set; }
+	[Reactive] public DateTimeOffset CreatedDate { get; set; }
+	[Reactive] public DateTimeOffset ModifiedDate { get; set; }
 
 	[ObservableAsProperty] public string? DisplayName { get; }
 	[ObservableAsProperty] public string? Version { get; }
 	[ObservableAsProperty] public Visibility HasThumbnail { get; }
+	[ObservableAsProperty] public string? CreatedDateText { get; }
+	[ObservableAsProperty] public string? ModifiedDateText { get; }
+	[ObservableAsProperty] public string? CreatedDateFullText { get; }
+	[ObservableAsProperty] public string? ModifiedDateFullText { get; }
 
 
 	private static readonly string[] _thumbnailImageTypes = [".png", ".jpg", ".jpeg"];
@@ -54,6 +61,10 @@ public class ProjectData : ReactiveObject
 					.FirstOrDefault();
 				ThumbnailFilePath = thumbnail;
 			}
+
+			var fileInfo = new FileInfo(ProjectMetaFilePath);
+			CreatedDate = fileInfo.CreationTime;
+			ModifiedDate = fileInfo.LastWriteTime;
 		}
 	}
 
@@ -99,6 +110,16 @@ public class ProjectData : ReactiveObject
 		}
 	}
 
+	private static string FormatDateTimeOffset(DateTimeOffset date, string format)
+	{
+		if (date == DateTimeOffset.MinValue || date == DateTimeOffset.MaxValue) return string.Empty;
+
+		return date.ToString(format, CultureInfo.CurrentCulture);
+	}
+
+	private static string FormatDateTimeOffsetShort(DateTimeOffset date) => FormatDateTimeOffset(date, "d");
+	private static string FormatDateTimeOffsetLong(DateTimeOffset date) => FormatDateTimeOffset(date, "f");
+
 	public ProjectData()
 	{
 		this.WhenAnyValue(x => x.ThumbnailFilePath).Subscribe(path =>
@@ -122,7 +143,28 @@ public class ProjectData : ReactiveObject
 		this.WhenAnyValue(x => x.Mod.VersionDisplayValue).Select(x => !string.IsNullOrEmpty(x) ? x : string.Empty)
 			.ToUIProperty(this, x => x.Version);
 
+		this.WhenAnyValue(x => x.Mod.FilePath).WhereNotNull().Subscribe(x =>
+		{
+			if(File.Exists(x))
+			{
+				try
+				{
+					var fileInfo = new FileInfo(x);
+					if(fileInfo.LastWriteTime > ModifiedDate) ModifiedDate = fileInfo.LastWriteTime;
+					if(fileInfo.CreationTime < CreatedDate) CreatedDate = fileInfo.CreationTime;
+				}
+				catch(Exception) { }
+			}
+		});
+
 		this.WhenAnyValue(x => x.Thumbnail).Select(x => x != null ? Visibility.Visible : Visibility.Collapsed)
 			.ToUIProperty(this, x => x.HasThumbnail, Visibility.Collapsed);
+
+		var whenCreated = this.WhenAnyValue(x => x.CreatedDate);
+		var whenModified = this.WhenAnyValue(x => x.ModifiedDate);
+		whenCreated.Select(FormatDateTimeOffsetShort).ToUIProperty(this, x => x.CreatedDateText, "");
+		whenCreated.Select(FormatDateTimeOffsetLong).ToUIProperty(this, x => x.CreatedDateFullText, "");
+		whenModified.Select(FormatDateTimeOffsetShort).ToUIProperty(this, x => x.ModifiedDateText, "");
+		whenModified.Select(FormatDateTimeOffsetLong).ToUIProperty(this, x => x.ModifiedDateFullText, "");
 	}
 }
